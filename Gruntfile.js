7e3209ee8c77cc9ffac547ede2ca85f4403b5233
Nick <npopov1992@gmail.com>
2018-09-06 23:35:00 +0500

    Grunt Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,16 @@
 module.exports = function(grunt) {
 
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin',
+        rev: 'grunt-file-rev'
+    });
 
     grunt.initConfig({
         sass: {
             dist: {
                 files: {
-                    'css/styles.css': 'css/styles.sccs'
+                    'css/styles.css': 'css/styles.scss'
                 }
             }
         },
@@ -29,9 +32,120 @@ module.exports = function(grunt) {
                     }
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [ {expand: true, src: ['*.html'], dest: 'dist' } ]
+            },
+            fonts : {
+                files: [ {expand: true, cwd:'node_modules/font-awesome', src: ['fonts/*.*'], dest: 'dist' } ]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [ {expand: true, src: ['img/*.{png,jpg,gif}'], dest: 'dist/'} ]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html', 'aboutus.html', 'index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        cssmin: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        rev: {
+            options: {
+                algorithm: 'md5',
+                length: 20
+            },
+            assets: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        // Usemin
+        // Replaces all assets with their revved version in html and css files.
+        // options.assetDirs contains the directories for finding the assets
+        // according to their relative paths
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html',
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html'
+                }
+            }
         }
+
     });
 
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'rev',
+        'usemin',
+        'htmlmin'
+    ]);
 };
